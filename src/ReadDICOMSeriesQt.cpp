cff751dd003e62828434227bf71a75f243efd132
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-03-16 11:12:51 +0100

    Filtering every slice

--- src/ReadDICOMSeriesQt.cpp
+++ src/ReadDICOMSeriesQt.cpp
@@ -7,6 +7,7 @@ ReadDICOMSeriesQt::ReadDICOMSeriesQt(QWidget *parent) : QMainWindow(parent), ui(
     ui->setupUi(this);
     reader = vtkSmartPointer<vtkDICOMImageReader>::New();
     viewer = vtkSmartPointer<vtkImageViewer2>::New();
+	viewerFilter = vtkSmartPointer<vtkImageViewer2>::New();
 }
 
 ReadDICOMSeriesQt::~ReadDICOMSeriesQt() {
@@ -19,27 +20,24 @@ void ReadDICOMSeriesQt::openDICOMFolder() {
 }
 
 void ReadDICOMSeriesQt::drawDICOMSeries(std::string folderDICOM) {
-    // Asigna carpeta de donde obtener las imágenes DICOM
     reader->SetDirectoryName(folderDICOM.c_str());
     reader->Update();
 
-    // Enlaza el input del viewer al output del reader
     viewer->SetInputConnection(reader->GetOutputPort());
 
-    // Enlaza el widget con el viewer
     ui->qvtkWidget->SetRenderWindow(viewer->GetRenderWindow());
+	ui->qvtkWidgetFilter->SetRenderWindow(viewerFilter->GetRenderWindow());
 
-    // Añade interactor al viewer
     viewer->SetupInteractor(ui->qvtkWidget->GetInteractor());
+	viewerFilter->SetupInteractor(ui->qvtkWidgetFilter->GetInteractor());
 
-    // Inicializa
     viewer->Render();
 
-    // Obtiene el número de slices
+	filter(0);
+
     minSlice = viewer->GetSliceMin();
     maxSlice = viewer->GetSliceMax();
 
-    // Actualiza labels de la GUI
     ui->sliderSlices->setMinimum(minSlice);
     ui->sliderSlices->setMaximum(maxSlice);
     ui->labelSlicesNumber->setText(QString::number(maxSlice - minSlice));
@@ -53,4 +51,54 @@ void ReadDICOMSeriesQt::on_buttonOpenFolder_clicked() {
 void ReadDICOMSeriesQt::on_sliderSlices_sliderMoved(int position) {
     viewer->SetSlice(position);
     viewer->Render();
+	filter(position);
+}
+
+void ReadDICOMSeriesQt::filter(int pos) {
+	VTKImageToImageType::Pointer connectorInput = VTKImageToImageType::New();
+
+	connectorInput->SetInput(reader->GetOutput());
+	connectorInput->Update();
+
+	Extract2DImageFilter::Pointer to2D = Extract2DImageFilter::New();
+	CastToRealFilterType::Pointer toReal = CastToRealFilterType::New();
+	CannyFilterType::Pointer filter = CannyFilterType::New();
+	RescaleFilterType::Pointer rescale = RescaleFilterType::New();
+
+	to2D->SetInput(connectorInput->GetOutput());
+	to2D->InPlaceOn();
+	to2D->SetDirectionCollapseToSubmatrix();
+	
+	CharImageType::RegionType inputRegion = connectorInput->GetOutput()->GetLargestPossibleRegion();
+	CharImageType::SizeType size = inputRegion.GetSize();
+	size[2] = 0;
+
+	CharImageType::IndexType start = inputRegion.GetIndex();
+	start[2] = pos;
+
+	CharImageType::RegionType desiredRegion;
+	desiredRegion.SetSize(size);
+	desiredRegion.SetIndex(start);
+
+	to2D->SetExtractionRegion(desiredRegion);
+	to2D->Update();
+
+	toReal->SetInput(to2D->GetOutput());
+	filter->SetInput(toReal->GetOutput());
+
+	filter->SetVariance(ui->doubleSpinBoxVariance->value());
+	filter->SetUpperThreshold(ui->doubleSpinBoxHigherThreshold->value());
+	filter->SetLowerThreshold(ui->doubleSpinBoxLowerThreshold->value());
+	filter->Update();
+	rescale->SetInput(filter->GetOutput());
+
+	ImageToVTKImageType::Pointer connectorOutput = ImageToVTKImageType::New();
+	connectorOutput->SetInput(rescale->GetOutput());
+	connectorOutput->Update();
+
+	viewerFilter->SetInputData(connectorOutput->GetOutput());
+
+	viewerFilter->Render();
+	viewerFilter->GetRenderer()->ResetCamera();
+	viewerFilter->Render();
 }
--- src/ReadDICOMSeriesQt.h
+++ src/ReadDICOMSeriesQt.h
@@ -9,6 +9,15 @@
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
 
+#include <itkImage.h>
+#include <itkImageFileReader.h>
+#include <itkImageToVTKImageFilter.h>
+#include <itkVTKImageToImageFilter.h>
+#include <itkCastImageFilter.h>
+#include <itkCannyEdgeDetectionImageFilter.h>
+#include <itkRescaleIntensityImageFilter.h>
+#include <itkExtractImageFilter.h>
+
 namespace Ui {
     class ReadDICOMSeriesQt;
 }
@@ -30,8 +39,28 @@ private:
     Ui::ReadDICOMSeriesQt *ui;
     vtkSmartPointer<vtkDICOMImageReader> reader;
     vtkSmartPointer<vtkImageViewer2> viewer;
+	vtkSmartPointer<vtkImageViewer2> viewerFilter;
+	vtkSmartPointer<vtkImageData> data = NULL;
     int minSlice;
     int maxSlice;
+
+	typedef signed short CharPixelType;
+	typedef float RealPixelType;
+
+	typedef itk::Image<CharPixelType, 3> CharImageType;
+	typedef itk::Image<RealPixelType, 3> RealImageType;
+	typedef itk::Image<CharPixelType, 2> Char2ImageType;
+	typedef itk::Image<RealPixelType, 2> Real2ImageType;
+
+	typedef itk::ExtractImageFilter<CharImageType, Char2ImageType> Extract2DImageFilter;
+	typedef itk::CastImageFilter<Char2ImageType, Real2ImageType> CastToRealFilterType;
+	typedef itk::CannyEdgeDetectionImageFilter<Real2ImageType, Real2ImageType> CannyFilterType;
+	typedef itk::RescaleIntensityImageFilter<Real2ImageType, Char2ImageType> RescaleFilterType;
+
+	typedef itk::ImageToVTKImageFilter<Char2ImageType> ImageToVTKImageType;
+	typedef itk::VTKImageToImageFilter<CharImageType> VTKImageToImageType;
+
+	void filter(int pos);
 };
 
 #endif // ReadDICOMSeriesQt_H
